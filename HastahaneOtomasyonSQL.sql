f9f7e38d3b6d94436667bdf541dc0aa77d5bead5
Taha Bozdemir <taha@bozdemir.net>
2022-12-24 22:02:04 +0300

    changes

--- HastahaneOtomasyonSQL.sql
+++ HastahaneOtomasyonSQL.sql
@@ -3,7 +3,7 @@ USE HastaneOtomasyonDB
 CREATE TABLE Kullanici(
 KullaniciID int identity PRIMARY KEY NOT NULL,
 KullaniciAdi varchar(25) NOT NULL,
-KullaniciSifre varchar(20) NOT NULL,
+KullaniciParola varchar(20) NOT NULL,
 KullaniciTur char(3) NOT NULL
 CONSTRAINT ck_KullaniciTur
 CHECK (KullaniciTur in ('DOK','VEZ','LAB'))
@@ -11,7 +11,7 @@ CHECK (KullaniciTur in ('DOK','VEZ','LAB'))
 
 CREATE TABLE Klinik(
 KlinikID tinyint identity PRIMARY KEY NOT NULL,
-KlinikAdi varchar(30) NOT NULL,
+KlinikAdi varchar(30) UNIQUE NOT NULL,
 )
 
 CREATE TABLE  Doktor(
@@ -20,25 +20,25 @@ CREATE TABLE  Doktor(
  CONSTRAINT fk_KlinikID_DoktorTB
  FOREIGN KEY (KlinikID) references Klinik(KlinikID) NOT NULL,
 
-DoktorTC tinyint UNIQUE NOT NULL
+DoktorTC bigint UNIQUE NOT NULL
 CONSTRAINT ck_DoktorTC
 CHECK(LEN(DoktorTC) = 11),
 
- DoktorAd varchar(25) NOT NULL,
- DoktorSoyad varchar(20) NOT NULL,
- DoktorCinsiyet char(1) NOT NULL
- CONSTRAINT ck_DoktorCinsiyet
- CHECK(DoktorCinsiyet in ('E', 'K')),
+DoktorAd varchar(25) NOT NULL,
+DoktorSoyad varchar(20) NOT NULL,
+DoktorCinsiyet char(1) NOT NULL
+CONSTRAINT ck_DoktorCinsiyet
+CHECK(DoktorCinsiyet in ('E', 'K')),
 
- DoktorDogumTarihi date 
- CONSTRAINT ck_DoktorDogumTarihi
- CHECK(DoktorDogumTarihi<=getdate()),
+DoktorDogumTarihi date 
+CONSTRAINT ck_DoktorDogumTarihi
+CHECK(DoktorDogumTarihi<=getdate()),
 )
 
 CREATE TABLE Hasta(
 HastaID int identity(1,1) PRIMARY KEY,
 
-HastaTC tinyint UNIQUE NOT NULL
+HastaTC bigint UNIQUE NOT NULL
 CONSTRAINT ck_HastaTC
 CHECK(LEN(HastaTC) = 11),
 
@@ -47,7 +47,7 @@ HastaSoyad varchar(20) NOT NULL ,
 HastaBabaAdi varchar(20),
 HastaAnneAdi varchar(20),
 
-HastaCepTel varchar(10) NOT NULL
+HastaCepTel bigint NOT NULL
 CONSTRAINT ck_HastaCepTel
 CHECK(LEN(HastaCepTel) = 10),
 
@@ -72,7 +72,7 @@ LabAdi varchar(50) NOT NULL
 CREATE TABLE Tani(
 TaniID int identity(1,1) PRIMARY KEY,
 TaniAdi varchar(50),
-TaniAciklama nvarchar(50)
+TaniAciklama nvarchar(max)
 )
 
 CREATE TABLE Test(
@@ -96,7 +96,7 @@ RandevuID int identity(1,1) PRIMARY KEY,
 
 HastaID int 
 CONSTRAINT fk_HastaID_RandevuTB
-FOREIGN KEY (HastaID) REFERENCES Hasta(HastasID),
+FOREIGN KEY (HastaID) REFERENCES Hasta(HastaID),
 
 RandevuDoktorID int 
 CONSTRAINT fk_RandevuDoktorID_RandevuTB
